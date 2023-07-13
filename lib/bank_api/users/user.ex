61c3ee9f9efbbf4013bdd4ec2db22c9cf16aa622
Marcos Flavio <mfghost69@gmail.com>
2023-07-12 21:43:52 -0300

    Finished creating the changeset for the User and encryption using Argon2

--- lib/bank_api/users/user.ex
+++ lib/bank_api/users/user.ex
@@ -2,8 +2,11 @@ defmodule BankApi.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @required_params [:name, :password, :email, :cep]
+
   schema "users" do
     field :name, :string
+    field :password, :string, virtual: true
     field :password_hash
     field :email, :string
     field :cep, :string
@@ -13,7 +16,17 @@ defmodule BankApi.User do
 
   def changeset(user \\ %__MODULE__{}, params) do
     user
-    |> cast(params, [:name, :password_hash, :email, :cep])
-    |> validate_required([:name, :password_hash, :email, :cep])
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
+    |> validate_length(:name, min: 3)
+    |> validate_length(:password, min: 6)
+    |> validate_length(:cep, min: 8)
+    |> validate_format(:email, ~r/@/)
+    |> add_password_hash()
   end
+
+  defp add_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, Argon2.add_hash(password))
+  end
+  defp add_password_hash(changeset), do: changeset
 end
