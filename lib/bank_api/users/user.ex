a53e22a60bb4ef1d4a537ba872d805dc6d8b6a24
Marcos Flavio <mfghost69@gmail.com>
2023-07-22 01:34:00 -0300

    Feat :Add documentation.

--- lib/bank_api/users/user.ex
+++ lib/bank_api/users/user.ex
@@ -1,4 +1,7 @@
 defmodule BankApi.Users.User do
+  @moduledoc """
+    Contains definition for the User schema and creates a changeset
+  """
   use Ecto.Schema
   import Ecto.Changeset
 
@@ -14,6 +17,11 @@ defmodule BankApi.Users.User do
     timestamps()
   end
 
+  @doc """
+    changeset/2
+    Receives an User struct and parameter to validate.
+    Performs param cast, params validation, adds password hash and returns a changeset
+  """
   def changeset(user \\ %__MODULE__{}, params) do
     user
     |> cast(params, @required_params)
